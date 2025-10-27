9266c44dfcb2b07f8dc9e923b3ca16f61e19d3c0
mfarhansheikh <mfaarhansheikh@gmail.com>
2025-10-27 19:27:14 +0500

    03 task compelet

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main (){
+    // TASK 01 //
+    List <String> names = ['M.farhan', 'M. salman ', 'Ariyan', 'Farooq' ];
+    print(names);
+
+    // TASK 02 //
+
+    List <String> days= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    print(days[6]);
+
+    // TASK 03// 
+    List data = ['Name : Muhammad Farhan', 'Class : BSCS','Roll no' '22590', 'Grade : A+', 'percentage' '86.00',  ];
+    print(data);
+}
