9dbfc8f126d8fb7721f590ab1214779f48344e50
mfarhansheikh <mfaarhansheikh@gmail.com>
2025-10-31 20:26:49 +0500

    Assignment 03 compelet.

--- main.dart
+++ main.dart
@@ -1,14 +1,48 @@
 void main (){
     // TASK 01 //
-    List <String> names = ['M.farhan', 'M. salman ', 'Ariyan', 'Farooq' ];
-    print(names);
+    // List <String> names = ['M.farhan', 'M. salman ', 'Ariyan', 'Farooq' ];
+    // print(names);
 
     // TASK 02 //
 
-    List <String> days= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    print(days[6]);
+    // List <String> days= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    // print(days[6]);
 
     // TASK 03// 
-    List data = ['Name : Muhammad Farhan', 'Class : BSCS','Roll no' '22590', 'Grade : A+', 'percentage' '86.00',  ];
-    print(data);
+    // List data = ['Name : Muhammad Farhan', 'Class : BSCS','Roll no' '22590', 'Grade : A+', 'percentage' '86.00',  ];
+    // print(data);
+
+    // TASK 04 //
+    // List<int> numbers = [ 45 , 20 , 10 , 40 , 50 , 60, 70, 80, 90, 20];
+    // int smallest=  numbers [0];
+    // int greatest = numbers[0];
+    // for (int number in numbers){
+    //     if (number < smallest){
+    //         smallest = number;
+    //     }
+    //     if ( number> greatest){
+    //         greatest = number;
+    //     }
+    // }
+    // print(20numbers);
+    // print ( "smallest number is: $smallest");
+    // print ("greatest number is: $greatest");
+
+    // TASK 05//
+
+    // List <int> value= [20 , 30, 40, 50, 60, 70 ];
+    // int maxvalue = value.reduce((int a,int b)=> a>b ? a : b);
+    // print("the maximum value is : $maxvalue");
+
+    //TASK 06//
+    // List <String> mylist= ['farhan' , 'Salman', 'farooq', 'usman', 'umer'];
+    // List <String> reversedlist = mylist.reversed.toList();
+    // print("Original List: $mylist");
+    // print("Reversed List: $reversedlist");
+
+    // TASK O8//
+     
+     List <String> usersEligibility= ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+     usersEligibility.removeAt(2);
+     print(usersEligibility);
 }
